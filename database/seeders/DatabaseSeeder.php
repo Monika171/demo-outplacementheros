f8aadf29ce3b60799e972b6e4c69d0c62bb81099
MonikaR17 <monikarabha171@gmail.com>
2020-10-27 10:20:57 +0900

    Added admin, role seeder table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Support\Facades\Schema;
+//use App\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        Schema::disableForeignKeyConstraints();
+
+        Role::truncate();
+
+        $this->call(UsersTableSeeder::class);
+        $this->command->info('The admin data has been seeded!');
+        Role::create(['name'=>'seeker']);  //Job Seeker
+        Role::create(['name'=>'employer']); //Hiring employer
+        Role::create(['name'=>'semployer']);  //Separating employer      
+        Role::create(['name'=>'volunteer']);  //Mentor Support Volunteer
+        Role::create(['name'=>'jvolunteer']);  //Job Search Support Volunteer
+        Role::create(['name'=>'consultant']);  //Consultant
+
+        $this->command->info('All other roles have been seeded!');
+
+        Schema::enableForeignKeyConstraints();
+       
+
     }
 }
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Role;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if(config('admin.admin_name')) {
+            $adminRole = Role::create(['name'=>'admin']);
+
+            $admin = User::firstOrCreate(
+                ['email' => config('admin.admin_email')], [
+                    'name' => config('admin.admin_name'),
+                    'password' => bcrypt(config('admin.admin_password')),
+                    'user_type'=>'admin',
+                    'email_verified_at'=>NOW(),
+
+                ]
+            );
+
+            $admin->roles()->attach($adminRole);
+            
+            /*$admin = User::create([
+                'name'=>'admin',
+                'user_type'=>'admin',
+                'email'=>'hello@outplacementheros.org',
+                'email_verified_at'=>NOW(),
+                'password'=>bcrypt('2020hired')
+                
+            ]);*/
+    
+            
+        }
+    }
+}
